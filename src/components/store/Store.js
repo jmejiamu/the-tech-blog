1eed648d4bcfc6cd240ffd3f1f197bed43f7852a
Jose <jmejiamu@gmail.com>
2021-02-02 01:35:05 -0800

    added end point to get the books

--- src/components/store/Store.js
+++ src/components/store/Store.js
@@ -1,68 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../NavBar';
+import restfulapi from '../URL/url';
 
 const Store = () => {
 
-    const cost = 12.5;
+    const [book, setBooks] = useState([]);
+    const [countN, setCount] = useState(0);
+
+    const getBooks = async () => {
+        try {
+            const data = await fetch(restfulapi.the_tech_blog + '/books');
+            const response = await data.json();
+            setBooks(response);
+
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getBooks();
+
+        // sessionStorage.setItem(save.id, JSON.stringify(save))
+
+    }, []);
+
+
+
+    let item = []
+    let itemObj = {}
+    // console.log(item);
+
+    const saveItemsAdded = (id, title, description, cost) => {
+        itemObj.id = id;
+        itemObj.title = title;
+        itemObj.description = description;
+        itemObj.cost = cost;
+        item.push({ itemObj })
+        // setSave(itemObj)
+        // setCount(countN)
+        sessionStorage.setItem(countN, JSON.stringify(item))
+        console.log(item);
+    }
     return (
         <>
             <NavBar />
             <div className="container">
+                <button type="button" className="button-style btn btn-link">Cart </button>
                 <div className="grid-container">
-                    <div className="card card-style mt-5 grid-item" >
-                        <img className="card-img-top" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Book Title</h5>
-                            <p className="card-text">Boook title description</p>
-                            <button type="button" className="button-style btn btn-link">Add Item</button>
-                            <p>$ {cost}</p>
-                        </div>
-                    </div>
-                    <div className="card card-style mt-5 grid-item" >
-                        <img className="card-img-top" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Book Title</h5>
-                            <p className="card-text">Boook title description</p>
-                            <button type="button" className="button-style btn btn-link">Add Item</button>
-                            <p>$ {cost}</p>
-                        </div>
-                    </div>
-                    <div className="card card-style mt-5 grid-item" >
-                        <img className="card-img-top" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Book Title</h5>
-                            <p className="card-text">Boook title description</p>
-                            <button type="button" className="button-style btn btn-link">Add Item</button>
-                            <p>$ {cost}</p>
-                        </div>
-                    </div>
-                    <div className="card card-style mt-5 grid-item mb-5" >
-                        <img className="card-img-top" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="bookimage" />
-                        <div className="card-body">
-                            <h5 className="card-title">Book Title</h5>
-                            <p className="card-text">Boook title description</p>
-                            <button type="button" className="button-style btn btn-link">Add Item</button>
-                            <p>$ {cost}</p>
-                        </div>
-                    </div>
-                    <div className="card card-style mt-5 grid-item mb-5" >
-                        <img className="card-img-top" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="bookimage" />
-                        <div className="card-body">
-                            <h5 className="card-title">Book Title</h5>
-                            <p className="card-text">Boook title description</p>
-                            <button type="button" className="button-style btn btn-link">Add Item</button>
-                            <p>$ {cost}</p>
-                        </div>
-                    </div>
-                    <div className="card card-style mt-5 grid-item mb-5" >
-                        <img className="card-img-top" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Book Title</h5>
-                            <p className="card-text">Boook title description</p>
-                            <button type="button" className="button-style btn btn-link">Add Item</button>
-                            <p>$ {cost}</p>
-                        </div>
-                    </div>
+                    {
+                        book.map((bookItems) => {
+                            return (
+                                <div key={bookItems.id} className="card card-style mt-5 grid-item mb-5" >
+                                    <img className="card-img-top" src={bookItems.book_url} alt="book-pic" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{bookItems.book_title}</h5>
+                                        <p className="card-text">{bookItems.book_description}</p>
+                                        <button type="button" onClick={() => saveItemsAdded(bookItems.id, bookItems.book_title, bookItems.book_description, bookItems.book_cost)} className="button-style btn btn-link">Add Item</button>
+                                        <p>$ {bookItems.book_cost}</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+
                 </div>
             </div>
         </>
